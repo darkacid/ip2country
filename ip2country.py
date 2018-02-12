d2dc742540d8b7df988c303edd4442052d96029c
Serg <sergstep314@gmail.com>
2018-02-12 10:38:30 +0400

    Update ip2country.py

--- ip2country.py
+++ ip2country.py
@@ -1,7 +1,7 @@
 import re
-import netaddr
+#import netaddr
 import csv
-
+#import ipaddress
 class ip2country:
     def __init__(self,dbPath):
         self.dbPath = dbPath
@@ -11,16 +11,42 @@ class ip2country:
         Return true if the first IP is greater than the second
         Return false otherwise.
         '''
-        firstList = firstIP.split(".")
-        secondList =secondIP.split(".")
+        #firstAddr = ipaddress.ip_address(firstIP)
+        #secondAddr = ipaddress.ip_address(secondIP)
+
+        #print(firstAddr>secondAddr)
+        #return (firstAddr>secondAddr)
+        try:
+
+            firstList = firstIP.split(".")
+            secondList =secondIP.split(".")
+        except ipSplit:
+            "Error converting given IPv4 address to list"
+
 
         if int(firstList[0]) > int(secondList[0]):
+
             return True
+        else:
+            if(int(firstList[0]) != int(secondList[0])):
+                return False
         if int(firstList[1]) > int(secondList[1]):
+
             return True
+        else:
+            if(int(firstList[1]) != int(secondList[1])):
+                return False
+        
         if int(firstList[2]) > int(secondList[2]):
+
             return True
+
+        else:
+            if(int(firstList[2]) != int(secondList[2])):
+                return False
+            
         if int(firstList[3]) > int(secondList[3]):
+
             return True
         return False
 
@@ -30,21 +56,22 @@ class ip2country:
         Given the ip address returns the country
         '''
         with open(self.dbPath,'r') as dbFile:
-            reader = csv.reader(dbFile)            
+            reader = csv.reader(dbFile)
+            if(self.compareIPs(ipAddr,"224.0.0.0")):
+                return "Unknown"
             for row in reader:
-                if (self.compareIPs(ipAddr,row[1])):                 
+                #1.7 sec
+                if (self.compareIPs(ipAddr,row[1])):          
                     continue
+                #Execute when Range End >= givenIP
 
-                if(row[0] == "0.0.0.0"):
-                    continue
-                if(row[0]=="::"):
-                    break
-                       
-                ipRange = netaddr.IPRange(row[0],row[1])             
-                if netaddr.IPAddress(ipAddr) in ipRange:            
-                    answer = self.short2long(row[2])
-                    print("Range contains : ",ipRange.size)
-                    return (answer)
+
+                answer = self.short2long(row[2])
+                return (answer)
+               # ipRange = netaddr.IPRange(row[0],row[1])             
+               # if netaddr.IPAddress(ipAddr) in ipRange:            
+               #     answer = self.short2long(row[2])                    
+               #     return (answer)
         return "Unknown"
 
     def short2long(self,shortCountryName):
